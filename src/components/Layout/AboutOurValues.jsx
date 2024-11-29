19e6af298dd68d2570c1555fcde44c84005eb94c
Abhilash CP <abhicplash@gmail.com>
2024-11-29 07:02:49 +0530

    about us page intro ,vision,mision completed

--- src/components/Layout/AboutOurValues.jsx
+++ src/components/Layout/AboutOurValues.jsx
@@ -1,10 +1,34 @@
-import React from 'react'
-import './AboutOurValues.css'
+import React from "react";
+import "./AboutOurValues.css";
+import vision from "../../Assets/about/vision.png";
+import mission from "../../Assets/about/mission.png";
 
 const AboutOurValues = () => {
   return (
-    <div className="AboutOurValuesContainer"></div>
-  )
-}
+    <div className="AboutOurValuesContainer">
+      <div className="visionCard">
+        <img src={vision} alt="" className="visionImg" />
+        <div className="visionDetails">
+          <span className="visionHead">Our Mission</span>
+          <span className="visionPara">
+            To deliver trusted services that enhance the functionality, safety,
+            and visual appeal of every space we serve.
+          </span>
+        </div>
+      </div>
+      <div className="visionCard">
+        <div className="visionDetails">
+          <span className="visionHead">Our Vision</span>
+          <span className="visionPara">
+          To become the preferred choice for technical and maintenance services in the UAE by consistently exceeding client
+          expectations.
+          </span>
+        </div>
+        <img src={mission} alt="" className="visionImg" />
+      </div>
+      
+    </div>
+  );
+};
 
-export default AboutOurValues
+export default AboutOurValues;
