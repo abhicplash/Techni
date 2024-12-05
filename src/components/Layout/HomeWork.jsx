28d5a5a39cab5590e152e10a2f9c90bce024df74
Abhilash CP <abhicplash@gmail.com>
2024-12-05 18:16:32 +0530

    home work and home service completed

--- src/components/Layout/HomeWork.jsx
+++ src/components/Layout/HomeWork.jsx
@@ -1,11 +1,30 @@
 import React from "react";
 import "./HomeWork.css";
+import { workProcess } from "../../utils/WorkProcess";
+import image from "../../Assets/services/reno.png";
 
 const HomeWork = () => {
   return (
     <div className="HomeWork-container">
-      <h1 className="homeserviceHead">our work process</h1>
-      
+      <span className="homeserviceHead">our work process</span>
+      <span className="processIntro">
+        At Techni Trust Technical Services, we follow a simple, efficient, and
+        transparent <br /> process to ensure the best outcomes for every project:
+      </span>
+      <div className="homeworkwrapper">
+        {/* <img src={image} alt="process" className="processImg" /> */}
+        <div className="processCardWrapper">
+          <div className="processList">
+            {workProcess.map((process) => (
+              <div key={process.id} className="processCard">
+                <h1>{process.id}</h1>
+                <span className="processHead">{process.name}</span>
+                <span className="processDesc">{process.desc}</span>
+              </div>
+            ))}
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
